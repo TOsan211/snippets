d86d95e9c45fefbf2ca2803bf98e4a78809ac977
TOsan211 <83643315+TOsan211@users.noreply.github.com>
2021-10-30 07:29:58 +0900

    Create isFunction.js

---
+++ isFunction.js
@@ -0,0 +1,11 @@
+/*
+* Checks if value is classified as `Function` Object
+* @params {any}
+* @returns {boolean}
+*/
+
+function inFunction(value){
+  return typeof value === 'function'
+}
+
+export default inFunction
