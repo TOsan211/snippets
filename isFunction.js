3b0c4fb35357a46e4f6bc76e32d252ca7bd6ff3e
TOsan211 <83643315+TOsan211@users.noreply.github.com>
2022-01-30 22:55:12 +0900

    Update isFunction.js

--- isFunction.js
+++ isFunction.js
@@ -3,9 +3,20 @@
 * @params {any}
 * @returns {boolean}
 */
+import isPrototype from './isPrototype.js';
 
 function isFunction(value){
-  return typeof value === 'function'
+  if(typeof value !== 'function') return false;
+  
+  if(!hasOwnProperty.call(value, 'length')) return false;
+  try{
+    if(typeof value.length !== 'number')   return false;
+    if(typeof value.call   !== 'function') return false;
+    if(typeof value.apply  !== 'function') return false;
+  } catch( error ) {
+    return false;
+  }
+  return !isPrototype(value);
 }
 
 export default isFunction
