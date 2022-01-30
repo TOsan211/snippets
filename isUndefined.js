c577f1d64a839dd19e674d30ea918bafa36db0ae
TOsan211 <83643315+TOsan211@users.noreply.github.com>
2022-01-30 21:50:06 +0900

    Create isUndefined.js

---
+++ isUndefined.js
@@ -0,0 +1,13 @@
+/**
+* Checks if value is undefined
+*
+* @params {any}
+* @returns {boolean}
+*
+*/
+var _undefined = void 0;
+
+function isUndifined(value){
+  return typeof value === _undefined;
+}
+export default isUndifined
